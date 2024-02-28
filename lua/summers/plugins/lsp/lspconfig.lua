232393b013cd0dcaf5948df7cf95a8a57336f9be
Bert W Summers <summersb@moesol.com>
2024-02-27 22:44:36 -0800

    more updates

--- lua/summers/plugins/lsp/lspconfig.lua
+++ lua/summers/plugins/lsp/lspconfig.lua
@@ -131,12 +131,6 @@ return {
       filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
     })
 
-    -- configure python server
-    lspconfig["pyright"].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-    })
-
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
